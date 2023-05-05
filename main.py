788f4b9206a19a6ac3f988467455b9cb39507b92
nurdaulet <ndusenbai@gmail.com>
2023-05-05 18:52:46 +0600

    Add birds image, table. Done 80%

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import sys
 import time
+import json
 
 
 class Player(pygame.sprite.Sprite):
@@ -68,11 +69,44 @@ class Bottom_kill(pygame.sprite.Sprite):
     def __init__(self, img):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load(img).convert_alpha(), (800, 50))
-        self.rect = self.image.get_rect(center=(400, 550))
+        self.rect = self.image.get_rect(center=(400, 600))
 
     def update(self, x, y):
         pass
 
+
+class Top_player:
+
+    def open_json_file(self):
+        with open('top_10.json', 'r') as file:
+            json_data = json.load(file)
+        return json_data
+
+    def list_value_order(self, json_file, new_element):
+        done_list = list(json_file.values())
+        done_list.append(new_element)
+        done_list.sort(reverse=True)
+        return done_list
+
+    def record(self, done_list):
+        done_json = {}
+
+        for value in range(10):
+            done_json[value] = done_list[value]
+        return done_json
+
+    def my_index(self, json, element):
+        done_list = list(json.values())
+        try:
+            ele_index = done_list.index(element)
+        except:
+            ele_index = None
+        return ele_index
+
+    def save_json(self, json_data):
+
+        with open('top_10.json', 'w') as f:
+            json.dump(json_data, f)
 
 
 if __name__ == '__main__':
@@ -85,12 +119,15 @@ if __name__ == '__main__':
     HEIGHT = 600
     STARTING_POINTS_LIST_VERTICAL_EGG = [240, 550]
     STARTING_POINTS_LIST_HORIZONTAL_EGG = [100, 300]
-    STARTING_POINTS_LIST_VERTICAL = [190, 500]
+    STARTING_POINTS_LIST_VERTICAL = [205, 515]
     STARTING_POINTS_LIST_HORIZONTAL = [180, 450]
-    point_x = 190
+    point_x = 215
     point_y = 180
-    LEVAL = [5, 10]
+    LEVAL = [15, 30]
     INTERVAL = 1000
+
+    #time
+    START_TIME = time.time()
 
     pygame.init()
     pygame.time.set_timer(pygame.USEREVENT, INTERVAL)
@@ -104,16 +141,22 @@ if __name__ == '__main__':
         EGGS_SURF.append(pygame.transform.scale(pygame.image.load(EGGS[i]).convert_alpha(), (50, 50)))
 
     # img load in pygame
-    bird_1 = pygame.image.load('img/birds/bird_1.png')
-    bird_2 = pygame.image.load('img/birds/bird_2.png')
-    bird_3 = pygame.image.load('img/birds/bird_3.png')
-    bird_4 = pygame.image.load('img/birds/bird_4.png')
+    bird_1 = pygame.transform.scale(pygame.image.load('img/birds/bird_1.png'), (80, 80))
+    bird_2 = pygame.transform.scale(pygame.image.load('img/birds/bird_2.png'), (80, 80))
+    bird_3 = pygame.transform.scale(pygame.image.load('img/birds/bird_3.png'), (80, 80))
+    bird_4 = pygame.transform.scale(pygame.image.load('img/birds/bird_4.png'), (80, 80))
+    egg_birds_left_top = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("img/other/egg_birds.png"), (170, 20)), -20)
+    egg_birds_left_bottom = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("img/other/egg_birds.png"), (170, 20)), -20)
+    egg_birds_right_top = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("img/other/egg_birds.png"), (170, 20)), 20)
+    egg_birds_right_bottom = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("img/other/egg_birds.png"), (170, 20)), 20)
 
     # Group
     eggs = pygame.sprite.Group()
 
     # Obj. class
-    obj_player = Player('img/basket.png')
+    obj_player = Player('img/other/basket.png')
+    obj_bottom_kill = Bottom_kill('img/other/bottom.png')
+    obj_top_player = Top_player()
 
     while game:
         for event in pygame.event.get():
@@ -154,7 +197,7 @@ if __name__ == '__main__':
         # text score
         text = font.render(f'Score: {SCORE}', True, (34, 139, 34), (25, 25, 112))
         obj_text_score = Write_text(font, text)
-        screen.blit(text, obj_text_score.score_text(80, 50))
+        screen.blit(text, obj_text_score.score_text(250, 50))
 
         #text leval
         if SCORE <= LEVAL[0]:
@@ -166,26 +209,63 @@ if __name__ == '__main__':
         obj_text_score = Write_text(font, text)
         screen.blit(text, obj_text_score.leval_text(600, 50))
 
-        #black rect. in bottom
-        hero = pygame.Surface((800, 50))
-        hero.fill((0, 0, 0))
-        rect = hero.get_rect()
-        screen.blit(hero, (0, 550))
-
         #build player image
         screen.blit(obj_player.image, obj_player.rect)
+        screen.blit(obj_bottom_kill.image, obj_bottom_kill.rect)
+
+        #build birds images
+        screen.blit(bird_1, (50, 50))
+        screen.blit(bird_2, (650, 50))
+        screen.blit(bird_3, (50, 250))
+        screen.blit(bird_4, (650, 250))
+        screen.blit(egg_birds_left_top, (50, 120))
+        screen.blit(egg_birds_left_bottom, (50, 320))
+        screen.blit(egg_birds_right_top, (580, 120))
+        screen.blit(egg_birds_right_top, (580, 320))
+
+        #kill
+        hit_player = pygame.sprite.spritecollide(obj_bottom_kill, eggs, True)
+        if hit_player:
+            END_TIME = time.time()
+            different_time = END_TIME - START_TIME
+
+            file = obj_top_player.open_json_file()
+            order_list = obj_top_player.list_value_order(file, different_time)
+            done_json = obj_top_player.record(order_list)
+            my_index = obj_top_player.my_index(done_json, different_time)
+            obj_top_player.save_json(done_json)
+            print(type(my_index))
+
+            screen.blit(background, (0, 0))
+
+            y_position = 50
+            incrementation = 0
+            for x, y in done_json.items():
+                if incrementation == my_index:
+                    text = font.render(f'{x}: {y}', True, (255, 0, 255), (25, 25, 112))
+                    obj_text_score = Write_text(font, text)
+                    screen.blit(text, obj_text_score.score_text(400, y_position))
+                    y_position += 50
+                else:
+                    text = font.render(f'{x}: {y}', True, (70, 130, 180), (25, 25, 112))
+                    obj_text_score = Write_text(font, text)
+                    screen.blit(text, obj_text_score.score_text(400, y_position))
+                    y_position += 50
+
+                incrementation += 1
+
+            pygame.display.update()
+
+            time.sleep(10)
+            game = False
 
         #egg score, sceed
         hit_player = pygame.sprite.spritecollide(obj_player, eggs, True)
         if hit_player:
             SCORE += 1
 
-            if SCORE <= LEVAL[0]:
-                SPEED = 6
-            elif LEVAL[0] < SCORE <= LEVAL[1]:
-                SPEED = 8
-            else:
-                SPEED = 13
+            if SCORE % 10 == 0:
+                SPEED += 4
 
         pygame.display.update()
         pygame.display.flip()
--- test/main.py
+++ test/main.py
@@ -1,66 +1,21 @@
-from random import randint
-import pygame as pg
-import sys
+import time
+import json
+def done_json():
+    new_value = 7
+    file = {1: 10, 2: 15, 3: 20, 4: 2, 5: 7, 6: 45, 7: 21, 8: 31, 9: 22, 10: 38}
+    done_json = {}
 
-pg.init()
-pg.time.set_timer(pg.USEREVENT, 3000)
+    value_list = list(file.values())
+    value_list.append(new_value)
+    value_list = sorted(value_list)
 
-W = 400
-H = 400
-WHITE = (255, 255, 255)
-CARS = ('bird_1.png', 'bird_2.png', 'bird_3.png', 'bird_4.png')
-# для хранения готовых машин-поверхностей
-CARS_SURF = []
+    for value in range(10):
+        done_json[value] = value_list[value]
 
-# надо установить видео режим
-# до вызова image.load()
-sc = pg.display.set_mode((W, H))
+    print(done_json)
 
-for i in range(len(CARS)):
-    CARS_SURF.append(
-        pg.image.load(CARS[i]).convert_alpha())
-
-
-class Car(pg.sprite.Sprite):
-    def __init__(self, x, surf, group):
-        pg.sprite.Sprite.__init__(self)
-        self.image = surf
-        self.rect = self.image.get_rect(
-            center=(x, 0))
-        # добавляем в группу
-        self.add(group)
-        # у машин будет разная скорость
-        self.speed = randint(1, 3)
-
-    def update(self):
-        if self.rect.y < H:
-            self.rect.y += self.speed
-        else:
-            # теперь не перебрасываем вверх,
-            # а удаляем из всех групп
-            self.kill()
-
-
-cars = pg.sprite.Group()
-
-# добавляем первую машину,
-# которая появляется сразу
-Car(randint(1, W),
-    CARS_SURF[randint(0, 2)], cars)
-
-while 1:
-    for i in pg.event.get():
-        if i.type == pg.QUIT:
-            sys.exit()
-        elif i.type == pg.USEREVENT:
-            Car(randint(1, W),
-                CARS_SURF[randint(0, 2)], cars)
-
-    sc.fill(WHITE)
-
-    cars.draw(sc)
-
-    pg.display.update()
-    pg.time.delay(20)
-
-    cars.update()
+if __name__ == '__main__':
+    with open('test.json', 'r') as f:
+        #data = f.read()
+        data = json.load(f)
+        print(data)
